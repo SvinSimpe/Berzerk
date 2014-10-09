0981a7f8d33016d946be66ef538c89d1bcb8f4a5
sege12 <sebastiangenlund@hotmail.com>
2014-10-09 13:20:05 +0200

    added Static texture

---
+++ GameLib/GameLib/StaticTexture.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLib
+{
+    class StaticTexture
+    {
+        Texture2D texture;
+        Vector2 position;
+
+        public Texture2D Texture
+        {
+            get { return texture; }
+        }
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public StaticTexture( Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.position = position;
+        }
+
+        void Update(GameTime gameTime)
+        { }
+
+        void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+        
+    }
+}
